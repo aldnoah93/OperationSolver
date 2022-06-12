9c4626c2b111d9de251a8c70930db50bc365223b
Mike Méndez <15219017+aldnoah93@users.noreply.github.com>
2022-06-12 16:39:00 -0600

    Adding simple unit test
    Adding unit test using Xunit

---
+++ OperationsSolver.Test/Logic/Operations/SumTest.cs
@@ -0,0 +1,34 @@
+﻿using OperationsSolver.Logic.Operations;
+
+namespace OperationsSolver.Test.Logic.Operations
+{
+    public class SumTest
+    {
+
+        [Theory]
+        [InlineData(new double[] { 1, 2, 3, 4, 5.5 }, 15.5)]
+        [InlineData(new double[] { 1, 2, 3, 4, 5 }, 15.0)]
+        [InlineData(new double[] { 1, 2, 3, 4 }, 10)]
+        public void Calculate_Sum_Successfully(IEnumerable<double> input,double expectedResult)
+        {
+            IOperation<double> operation = new Sum();
+
+            var result = operation.Calculate(input);
+
+            Assert.Equal<double>(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(new double[] { 1, 2, 3, 4, 5.5 }, 16.5)]
+        [InlineData(new double[] { 1, 2, 3, 4, 5 }, 16.0)]
+        [InlineData(new double[] { 1, 2, 3, 4 }, 11)]
+        public void Calculate_Sum_Not_Successfully(IEnumerable<double> input, double expectedResult)
+        {
+            IOperation<double> operation = new Sum();
+
+            var result = operation.Calculate(input);
+
+            Assert.NotEqual<double>(expectedResult, result);
+        }
+    }
+}
